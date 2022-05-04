13a5831643fb514b04da8a3d20dab1fe1ae0e9ac
DeeepshikhaRaghuvanshi <ddeepshikha.raghuvanshi@gmail.com>
2022-05-04 18:44:46 +0530

    added dependencies and main file

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const expres = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const route = require('./routes/route.js')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+  .connect(
+    "mongodb+srv://project1:project1%40123@cluster0.cnmul.mongodb.net/group83Database?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() =>
+    console.log("MongoDB is connected / Cluster0 / project1db")
+  )
+  .catch((err) => console.log(err));
+
+  app.use("/", route);
+
+  app.listen(process.env.PORT || 3000, function () {
+    console.log("Express app running on port " + (process.env.PORT || 3000));
+  });
+  
