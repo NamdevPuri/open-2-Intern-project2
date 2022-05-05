80bcd455316e3bae289494325ebff5541eb73b69
namdevpuri3@gmail.com <namdevpuri3@gmail.com>
2022-05-05 20:56:37 +0530

    project2update

--- src/Controllers/collegeController.js
+++ src/Controllers/collegeController.js
@@ -16,37 +16,22 @@ const getCollege = async function (req, res) {
   try {
     let collegeName = req.query.collegeName;
     const collegeNames = await collegeModel.findOne({ name: collegeName });
-    if (!collegeNames) {
-      return res
-        .status(404)
-        .send({
-          status: false,
-          message: "College Not Found ",
-        });
+    if (!collegeNames) {return res.status(404).send({status: false, message: "College Not Found "});
     }
 
     const collegeId = collegeNames._id;
 
-    const interns = await internModel
-      .find({ collegeId: collegeId })
-      .select({ _id: 1, email: 1, name: 1, mobile: 1 });
+    const interns = await internModel.find({ collegeId: collegeId }).select({ _id: 1, email: 1, name: 1, mobile: 1});
 
     const { name, fullName, logolink } = collegeNames;
 
     // Final list of College details with students name who applied for internship
 
-    const finalData = {
-      name: name,
-      fullName: fullName,
-      logolink: logolink,
-      interests: interns.length
-        ? interns
+    const finalData = {name: name, fullName: fullName, logolink: logolink, interests: interns.length ? interns
         : { message: "No one applied for internship in this college" },
     };
+ return res.status(200).send({ status: true, data: finalData });
 
-    return res
-      .status(200)
-      .send({ status: true, data: finalData });
   } catch (err) {
     console.log("This is the error : ", err.message);
     res.status(500).send({ msg: "Error", error: err.message });
--- src/Controllers/internController.js
+++ src/Controllers/internController.js
@@ -8,43 +8,26 @@ const createIntern = async function (req, res) {
 
     let numberCheck = await internModel.findOne({ mobile: data.mobile });
 
-    if (numberCheck)
-      return res
-        .status(400)
-        .send({ status: false, msg: "Mobile Number Already Exists" });
+    if (numberCheck) 
+    return res.status(400).send({ status: false, msg: "Mobile Number Already Exists" });
 
     let emailCheck = await internModel.findOne({ email: data.email });
 
     if (emailCheck)
-      return res
-        .status(400)
-        .send({ status: false, msg: "EmailID Already Exists" });
+      return res.status(400).send({ status: false, msg: "EmailID Already Exists" });
 
-    let collegeCheck = await collegeModel.findOne({
-      name: data.collegeName,
-      isDeleted: false,
-    });
+    let collegeCheck = await collegeModel.findOne({ name: data.collegeName, isDeleted: false,});
 
    
     if (!collegeCheck) {
-      return res.status(400).send({
-        status: false,
-        message: `${data.collegeName} college doesn't exists.`,
-      });
+      return res.status(400).send({ status: false, message: `${data.collegeName} college doesn't exists.`});
     }
     console.log(data)
     let collegeId = collegeCheck._id;
     data.collegeId = collegeId;
 
     const internData = await internModel.create(data);
-
-    return res
-      .status(201)
-      .send({
-        status: true,
-        message: `Successfully applied for internship at ${data.collegeName}.`,
-        data: internData,
-      });
+    return res.status(201).send({status: true,message: `Successfully applied for internship at ${data.collegeName}.`, data: internData,});
   } catch (err) {
     console.log("This is the error :", err.message);
     res.status(500).send({ msg: "Error", error: err.message });
